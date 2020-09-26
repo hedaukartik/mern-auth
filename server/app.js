8bff2a9ad10ceacf9ff5f4e789e40f3ee8400bfd
hedaukartik <hedaukartik2009@gmail.com>
2020-09-26 19:49:31 +0530

    BE working with jwt auth

--- server/app.js
+++ server/app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const morgan = require("morgan"); //for development purpose to get the routes requested in console
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/connectDB");
 
 //import routes
-// const authRoutes = require("./backend/routes/auth");
-// const userRoutes = require("./backend/routes/user");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 //app
 const app = express();
@@ -20,11 +21,12 @@ connectDB();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
 app.use(cors());
 
 //routes middleware
-// app.use("/api", authRoutes);
-// app.use("/api", userRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
