5f3757fb1af226ae444900e1ae33a12cbc7f761e
hedaukartik <hedaukartik2009@gmail.com>
2020-09-27 18:25:23 +0530

    FE Activation component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import SignUp from "./components/SignUp";
+import { Activation } from "./components/Activation";
 
 function App() {
     return (
         <div>
             <Switch>
                 <Route exact path="/" />
-                <Route path="/signup" component={SignUp} />
+                <Route exact path="/signup" component={SignUp} />
+                <Route
+                    exact
+                    path="/users/activate/:token"
+                    component={Activation}
+                />
             </Switch>
         </div>
     );
